801ef73fc8d9533c0905c226b381e2d98f92066d
HilmiKhaidar <Hilmikhaidarnabilla@gmail.com>
2025-04-30 15:08:08 -0700

    Modul 10 - Arrow Function dan Anonim Function

---
+++ Modul10 - index.js
@@ -0,0 +1,9 @@
+// Arrow function
+const tambah = (a, b) => a + b;
+console.log(tambah(3, 4)); // 7
+
+// Fungsi anonim
+let kali = function(x, y) {
+  return x * y;
+};
+console.log(kali(5, 2)); // 10
