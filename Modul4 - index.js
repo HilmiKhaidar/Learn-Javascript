dec144ae2c625230d14f6f8d98dbc3bdeed0522f
HilmiKhaidar <Hilmikhaidarnabilla@gmail.com>
2025-04-30 13:25:22 -0700

    Modul 4: String dan Template Literals

---
+++ Modul4 - index.js
@@ -0,0 +1,6 @@
+let nama = "Mikhaii";
+let salam = `Halo, nama saya ${nama}`;
+console.log(salam); // Halo, nama saya Mikhaii
+
+console.log(nama.length); // Panjang string
+console.log(nama.toUpperCase()); // Uppercase
