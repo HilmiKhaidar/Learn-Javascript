0310d9c6813c1437f9ef96a2b0abbad8cb1c886e
HilmiKhaidar <Hilmikhaidarnabilla@gmail.com>
2025-04-30 15:05:34 -0700

    Modul 9 - Fungsi (Function)

---
+++ Modul9 - index.js
@@ -0,0 +1,13 @@
+function sapa(nama) {
+    return `Halo, ${nama}!`;
+  }
+  
+  let hasil = sapa("Mikhaii");
+  console.log(hasil); // Halo, Mikhaii!
+  
+  function nyapa (Ngaran) {
+    return `Halo, ${Ngaran}!`;
+  }
+
+  let ieuhasil = nyapa("hilmi");
+  console.log(ieuhasil);
