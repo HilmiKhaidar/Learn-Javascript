26898c1bb654267feb62253957d7fae9d778b29e
HilmiKhaidar <Hilmikhaidarnabilla@gmail.com>
2025-04-30 13:28:08 -0700

    Modul 5 -  Array: Pengenalan dan Manipulasi Dasar

---
+++ Modul5 - index.js
@@ -0,0 +1,6 @@
+let buah = ["apel", "jeruk", "pisang"];
+buah.push("mangga"); // Tambah
+buah.pop();          // Hapus terakhir
+console.log(buah[1]); // Akses indeks ke-1
+console.log(buah.length); // Panjang array
+console.log(buah);
