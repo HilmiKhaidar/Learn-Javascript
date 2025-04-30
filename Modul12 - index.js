fc55ac665eaad9d3818b416939dc0150e805e32b
HilmiKhaidar <Hilmikhaidarnabilla@gmail.com>
2025-04-30 15:21:41 -0700

    Modul 12 - Destructuring

---
+++ Modul12 - index.js
@@ -0,0 +1,14 @@
+// Array
+let warna = ["merah", "hijau", "biru"];
+let [warna1, warna2] = warna;
+console.log(warna1); // merah
+console.log(warna2); // hijau
+
+// Object
+let user = {
+  nama: "Mikhaii",
+  umur: 21
+};
+let { nama, umur } = user;
+console.log(nama); // Mikhaii
+console.log(umur); // 21
