ece4d3e36f5f6c5c5ed13515fcbe6953eb63033a
HilmiKhaidar <Hilmikhaidarnabilla@gmail.com>
2025-04-30 21:06:04 -0700

    Modul 19 - Modules (import/export)

---
+++ Modul19 - index.js
@@ -0,0 +1,9 @@
+// file math.js
+// export function tambah(a, b) {
+//     return a + b;
+//   }
+  
+  // file main.js
+  import { tambah } from './Modul19.1 - math.js';
+  console.log(tambah(5, 3)); // 8
+  
